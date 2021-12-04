4b786d681fd28d246673acee7ae13d5df8434760
Xiagir <513535b@gmail.com>
2021-12-03 20:10:32 -0500

    Implemented a new design for test page / edited login and signup

--- index.js
+++ index.js
@@ -29,23 +29,21 @@ app.listen(PORT, () => {
   console.log("Running Server On Port: " + PORT);
 });
 
-
 // and now You can use 2.x express dynamicHelpers
-require('express-dynamic-helpers-patch')(app);
+require("express-dynamic-helpers-patch")(app);
 
 app.dynamicHelpers({
-    loggedIn: function (req, res) {
-      return req.session.loggedIn;
-    }
- });
+  loggedIn: function (req, res) {
+    return req.session.loggedIn;
+  },
+});
 
 app.use(cookieParser());
-app.use(session({secret: 'secret', resave: false, saveUninitialized: true}));
+app.use(session({ secret: "secret", resave: false, saveUninitialized: true }));
 
 app.use("/", userAuth);
 app.use("/questions", questions);
 app.use("/profile/", userCabinet);
-
 
 //connect pug files
 app.set("views", path.join(__dirname, "views"));
@@ -55,4 +53,20 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+const animateCSS = (element, animation, prefix = "animate__") =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    const node = document.querySelector(element);
 
+    node.classList.add(`${prefix}animated`, animationName);
+
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve("Animation ended");
+    }
+
+    node.addEventListener("animationend", handleAnimationEnd, { once: true });
+  });
