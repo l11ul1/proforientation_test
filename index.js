379447cbe8d77f961c057a5c26c6b6e440364330
Vladislav Mun <welastor@gmail.com>
2022-02-02 23:12:01 -0500

    done

--- index.js
+++ index.js
@@ -20,22 +20,22 @@ const session = require("express-session");
 
 const PORT = 8888;
 const url =
-  "mongodb+srv://admin:3989302As@cluster0.o2m8r.mongodb.net/prof_orientation_test_db?retryWrites=true&w=majority";
+	"mongodb+srv://admin:3989302As@cluster0.o2m8r.mongodb.net/prof_orientation_test_db?retryWrites=true&w=majority";
 const connectionOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 const Schema = mongoose.Schema;
 
 //create a server
 app.listen(PORT, () => {
-  console.log("Running Server On Port: " + PORT);
+	console.log("Running Server On Port: " + PORT);
 });
 
 // and now You can use 2.x express dynamicHelpers
 require("express-dynamic-helpers-patch")(app);
 
 app.dynamicHelpers({
-  loggedIn: function (req, res) {
-    return req.session.loggedIn;
-  },
+	loggedIn: function (req, res) {
+		return req.session.loggedIn;
+	},
 });
 
 app.use(cookieParser());
@@ -50,23 +50,25 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  res.render("index");
+	res.render("index");
 });
 
 const animateCSS = (element, animation, prefix = "animate__") =>
-  // We create a Promise and return it
-  new Promise((resolve, reject) => {
-    const animationName = `${prefix}${animation}`;
-    const node = document.querySelector(element);
+	// We create a Promise and return it
+	new Promise((resolve, reject) => {
+		const animationName = `${prefix}${animation}`;
+		const node = document.querySelector(element);
 
-    node.classList.add(`${prefix}animated`, animationName);
+		node.classList.add(`${prefix}animated`, animationName);
 
-    // When the animation ends, we clean the classes and resolve the Promise
-    function handleAnimationEnd(event) {
-      event.stopPropagation();
-      node.classList.remove(`${prefix}animated`, animationName);
-      resolve("Animation ended");
-    }
+		// When the animation ends, we clean the classes and resolve the Promise
+		function handleAnimationEnd(event) {
+			event.stopPropagation();
+			node.classList.remove(`${prefix}animated`, animationName);
+			resolve("Animation ended");
+		}
 
-    node.addEventListener("animationend", handleAnimationEnd, { once: true });
-  });
+		node.addEventListener("animationend", handleAnimationEnd, {
+			once: true,
+		});
+	});
